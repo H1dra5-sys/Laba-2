515f3dadae10ce8cba322c0535fa2cbc4ecbf21e
H1dra5-sys <H1dra5@yandex.ru>
2025-09-26 10:24:48 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,37 +3,39 @@
 * Название: Вариант 9  *
 ************************/
 
-
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 int main() {
     
-  double C0,D,pow1,e,c,pow2,C;
-  int x,t;
+  double C0, D, e, pow2, C;
+  int x, t, step1, step2, lim1, lim2;
     
   C0 = 5.73;
   t = 3600;
-  pow1 = pow(10, -4);
-  D = 6 * pow1;
+  D = 6.0 * pow(10.0,-4.0);
   e = 2.7;
   x = 1;
+  step1 = 1;
+  step2 = 5;
+  lim1 = 5;
+  lim2 = 30;
     
     do {
-      pow2 = pow(2.7,  (-x * x) / (4 * D * t));
+      pow2 = pow(e,  (-x * x) / (4.0 * D * t));
       C = C0 * pow2;
-      cout << "C = " << C << endl;
-      cout << "x = " << x << endl;
-      x += 1;
-    } while (x != 5);
+      cout << "C = " << C << endl
+      << "x = " << x << endl;
+      x += step1;
+    } while (x < lim1);
     
       do {
-        pow2 = pow(2.7,  (-x * x) / (4 * D * t));
+        pow2 = pow(e,  (-x * x) / (4.0 * D * t));
         C = C0 * pow2;
-        cout << "C = " << C << endl;
-        cout << "x = " << x << endl;
-        x += 5;
-      } while (x != 30);
+        cout << "C = " << C << endl
+        << "x = " << x << endl;
+        x += step2;
+      } while (x < lim2);
     
 }
